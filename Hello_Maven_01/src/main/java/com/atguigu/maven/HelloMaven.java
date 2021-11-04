04b45008e0c33424670a3339e24270fc2892c083
LaughingTiger <624597867@qq.com>
2021-11-04 10:41:05 +0800

    Initial commit

---
+++ Hello_Maven_01/src/main/java/com/atguigu/maven/HelloMaven.java
@@ -0,0 +1,13 @@
+package com.atguigu.maven;
+
+/**
+ * @author CJYong
+ * @create 2021-09-06 15:58
+ */
+public class HelloMaven {
+
+    public void sayHello(String name){
+        System.out.println("Hello, " + name + "!");
+    }
+
+}
---
+++ Hello_Maven_01/src/test/java/com/atguigu/maven/TestHelloMaven.java
@@ -0,0 +1,16 @@
+package com.atguigu.maven;
+
+import org.junit.Test;
+
+/**
+ * @author CJYong
+ * @create 2021-09-06 16:00
+ */
+public class TestHelloMaven {
+
+    @Test
+    public void test1(){
+        HelloMaven helloMaven = new HelloMaven();
+        helloMaven.sayHello(" C J Y ");
+    }
+}
